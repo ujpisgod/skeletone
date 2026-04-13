8c2d7e1d87414d51ba5f4204427f088c9259339f
ujpisgod <ujaepum@gmail.com>
2026-04-14 00:18:13 +0900

    feat: 첫번째 레이어 완성

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import ops as mm
-a,b=map(int,input("첫번째 행렬의 행과 열의 크기를 입력하세요 (예: 3 4): ").split())
-ma1=mm.create_z_mat(a,b)
+a,b,c=map(int,input('3개의 숫자를 띄워쓰기해 입력해 주세요').split())
+ma1=mm.create_r_mat(a,b)
 for i in ma1:
     print(*i)
-a,b=map(int,input("두번째 행렬의 행과 열의 크기를 입력하세요 (예: 3 4): ").split())
-ma2=mm.create_z_mat(a,b)
+print(f'{a}곱하기 {b} 사이즈의 행렬입니다\n')
+ma2=mm.create_r_mat(b,c)
 for i in ma2:
     print(*i)
+print(f'{b}곱하기 {c} 사이즈의 행렬입니다\n')
 kk=mm.mat_mul(ma1,ma2)
-print('\n')
+print('\n 다음은 곱해진 행렬입니다 \n')
 for i in kk:
     print(*i)
+print('\n 곱한 행렬에 편향를 더합시다')
+a2,b2=mm.ismat(kk)
+fisrt_random=mm.create_r_mat(a2,b2)
+tt=mm.mat_add(kk,fisrt_random)
+for i in tt:
+    print(*i)
+print('\n 위 행렬은 랜덤 편향이 더해진 행렬입니다\n')
+tt2=mm.relu(tt)
+for i in tt2:
+    print(*i)
+print('\n 위 행렬은 랜덤 편향이 더해진 행렬이 레루 함수를 통과한 행렬입니다')
--- ops.py
+++ ops.py
@@ -3,26 +3,50 @@ def create_z_mat(a,b):
     for i in range(a):
         l.append([0]*b)
     return l
-def mat_mul(a,b):
+def ismat(a):
     if a and a[0]:
         n=len(a)
         k1=len(a[0])
     else:
-        raise ValueError("1번 행렬이 비어있습니다.")
-    if b and b[0]:
-        k2=len(b)
-        m=len(b[0])
-    else:
-        raise ValueError("2번 행렬이 비어있습니다.")
+        raise ValueError("행렬이 비어있습니다.")
+    return n,k1
+def create_r_mat(a,b):
+    import random
+    l=[[] for _ in range(a)]
+    for i in range(a):
+        for j in range(b):
+            l[i].append(random.uniform(-1, 1))
+    return l
+def mat_mul(a,b):
+    aa,k1=ismat(a)
+    k2,bb=ismat(b)
     if k1==k2:
         k=k1
     else:
         raise ValueError("행렬의 내항 차원이 일치하지 않습니다.")
-    result=[[] for i in range(n)]
-    for i in range(n):
-        for j in range(m):
+    result=[[] for i in range(aa)]
+    for i in range(aa):
+        for j in range(bb):
             d=0
             for x in range(k):
                d+=a[i][x]*b[x][j] 
             result[i].append(d)
+    return result
+def mat_add(a,b):
+    a1,a2=ismat(a)
+    b1,b2=ismat(b)
+    if a1==b1 and a2==b2:
+        c=[[] for i in range(a1)]
+        for i in range(a1):
+            for j in range(a2):
+                c[i].append(a[i][j]+b[i][j])
+        return c
+    else:
+        raise ValueError('행과 열이 맞지 않는 행렬끼리는 더할 수 없습니다')
+def relu(a):
+    n,m=ismat(a)
+    result=[[] for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            result[i].append(max(0,a[i][j]))
     return result
